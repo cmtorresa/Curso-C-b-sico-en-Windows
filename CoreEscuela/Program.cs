1ac42542ce08c6d523df40ce304309eb8f8cd871
cmtorresa <cesarmta87@gmail.com>
2022-10-22 05:19:03 -0500

    This is my first project in C#, my first commit with this language

---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,30 @@
+﻿class Escuela
+{
+    public string? nombre;
+    public string? direccion;
+
+    public int year;
+    public string? ceo;
+
+    public void Timbrar() 
+    {
+       Console.Beep(1000,3000);
+
+    }
+    
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var miEscuela = new Escuela();
+        miEscuela.nombre = "Platzi Academy";
+        miEscuela.direccion = "Al lado del que vende casa";
+        miEscuela.year = 1987;
+        miEscuela.ceo= "Freddy vega";
+        Console.WriteLine("Esta timbando la consola!!!");
+        miEscuela.Timbrar();
+
+        //Console.WriteLine("Hello, World!");
+    }
+}
