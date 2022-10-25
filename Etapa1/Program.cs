5a7e01410a5374602e56d31184d046ccf9acc3c6
cmtorresa <cesarmta87@gmail.com>
2022-10-25 06:16:47 -0500

    Created a new kind of courses for the school. The next part, I Will fix the quantity with an array

--- Etapa1/Program.cs
+++ Etapa1/Program.cs
@@ -6,7 +6,21 @@ internal class Program
     private static void Main(string[] args)
     {
         var escuela = new Escuela("Platzi School", 2012,TiposEscuela.primaria, pais:"Colombia", ciudad:"Bogotá");
-        
+        var curso1 =new Curso() {
+            Nombre="101"
+        };
+        var curso2 =new Curso() {
+            Nombre="201"
+        };
+        var curso3 =new Curso() {
+            Nombre="301"
+        };
         Console.WriteLine(escuela);
+        System.Console.WriteLine("===============================");
+        Console.WriteLine(curso1.Nombre + ", " + curso1.UniqueId);
+        System.Console.WriteLine("===============================");
+        Console.WriteLine(curso2.Nombre + ", " + curso2.UniqueId);
+        System.Console.WriteLine("===============================");
+        Console.WriteLine(curso3.Nombre + ", " + curso3.UniqueId);
     }
 }
